b26b03347d74cf5077ff1e63c31a4dd069296ea1
Gian Clarke <gianclarke@hotmail.com>
2022-05-05 09:27:50 +1200

    created template

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once('include/init.php');
 if ($session->is_signed_in()) {
     $user = User::find_user_by_id($_SESSION['user_id']);
 }
-echo $user->first_name;
+// echo $user->first_name;
 
 ?>
 
@@ -11,69 +11,119 @@ echo $user->first_name;
 <html lang="en">
 
 <head>
-    <title>Bootstrap Example</title>
+    <title>@yield("page-title")</title>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" />
 
 </head>
 
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <nav class="navbar">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                            </button>
-                            <a class="navbar-brand" href="#">Logo</a>
-                        </div>
-                        <div class="collapse navbar-collapse" id="myNavbar">
-                            <ul class="nav navbar-nav">
-                                <li class="active"><a href="#">Home</a></li>
-                                <li><a href="#">About</a></li>
-                                <li><a href="#">Projects</a></li>
-                                <li><a href="#">Contact</a></li>
-                            </ul>
-                            <ul class="nav navbar-nav navbar-right">
-                                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                            </ul>
+    <div id="app">
+        <div class="container-fluid p-2 bg-white border-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-4">
+                        <a href="index.php">
+                            <img class="header__logo" src="images/place-holder-logo.png" alt="">
+                        </a>
+                    </div>
+                    <div class="col-sm-8 pt-1">
+                        <ul class="nav float-end">
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="">
+                                    <i class="fa-solid fa-cart-shopping"></i>
+                                    Cart
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="">
+                                    <i class="fa-solid fa-binoculars"></i>
+                                    Watchlist
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="">
+                                    <i class="fa-solid fa-pencil"></i>
+                                    Start a listing
+                                </a>
+                            </li>
+                            <?php if (!$session->is_signed_in()) { ?>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="login.php">Login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link text-secondary" href="">Register</a>
+                            </li>
+                            <?php } else { ?>
+                            <li class="nav-item">
+                                <a href="" class="nav-link">
+                                    <i class="fa-solid fa-user"></i>
+                                    My Account
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="logout.php" class="text-white">
+                                    <i class="fa-solid fa-right-from-bracket"></i>
+                                    Logout
+                                </a>
+                            </li>
+                            <?php } ?>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container-fluid header pb-4 pt-4">
+            <div class="container text-white">
+                <div class="row align-items-end">
+                    <div class="col-sm-8">
+                        <div class="row">
+                            <div class="card-body row align-items-end">
+                                <div class="col">
+                                    <h3 class="pb-1">New Zealands #1 FREE Online Auction and Classifieds</h1>
+                                        <h1 class="pb-1">What are you looking for today?</h1>
+
+                                        <div class="search">
+                                            <input type="text" class="form-control" placeholder="Search all classifieds">
+                                            <button class="btn btn-primary search__button">Search</button>
+                                        </div>
+
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </nav>
+                    <div class="col-sm-4">
+                        <img class=" align-top img-responsive" src="images/test.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container-fluid pt-3 pb-5">
+            <div class="container">
+                Content here!
             </div>
         </div>
 
 
-        <h3>What We Do</h3><br>
-        <div class="row">
-            <div class="col-sm-4">
-                <p>Current Project</p>
-            </div>
-            <div class="col-sm-4">
-                <p>Project 2</p>
-            </div>
-            <div class="col-sm-4">
-                <div class="well">
-                    <p>Some text..</p>
-                </div>
-                <div class="well">
-                    <p>Some text..</p>
-                </div>
-            </div>
+
+
+        <div class="container-fluid p-5 bg-secondary text-white text-center">
+            <h1>Footer</h1>
+            <p>Resize this responsive page to see the effect!</p>
         </div>
-    </div><br>
 
-    <footer class="container-fluid text-center">
-        <p>Footer Text</p>
-    </footer>
 
+    </div>
 </body>
 
 </html>
--- logout.php
+++ logout.php
@@ -3,3 +3,9 @@ require_once('include/init.php');
 $session->logout();
 redirect('login.php')
 ?>
+<?php
+if ($session->is_signed_in()) {
+    $user = User::find_user_by_id($session['user_id']);
+}
+
+?>
